a25237a6b2d5d0c54ca9aa532a376cf5f1db52b3
matin-azami <matin.azami.03@gmail.com>
2022-07-05 14:38:32 +0430

    Ex

---
+++ 1-Base/7-MohasebatSakht.py
@@ -0,0 +1,5 @@
+from cmath import tan
+import math
+x = int(input())
+math.gcd(math.ceil(math.pow(x,1.0*5/3) + math.tan(math.radians(x))), 
+         math.floor(math.pow(math.pi, (2+math.atan(math.pow(math.sin(math.radians(x)),2))))))
